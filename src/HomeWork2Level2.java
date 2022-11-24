e51839ee49cf90b3899b4dd9ae9b26fc27ba40c8
par55 <par555@mail.ru>
2022-11-24 16:10:43 +0100

    Initial commit

---
+++ src/HomeWork2Level2.java
@@ -0,0 +1,7 @@
+public class HomeWork2Level2 {
+    public static void main(String[] args) {
+        int apples = 14;
+        int pupils = 3;
+        System.out.println( 14 % 3 );
+    }
+}
---
+++ src/HomeWork2level1.java
@@ -0,0 +1,6 @@
+public class HomeWork2level1 {
+    public static void main(String[] args) {
+        int myAge = 5;
+        System.out.println( ( myAge * 3 + 2 ) / 2 );
+    }
+}
